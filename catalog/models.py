e824d032be61868d8d66613bd284a85d25804223
KYD <kydkang@gmail.com>
2018-06-18 11:17:55 +0700

    After Part 6

--- catalog/models.py
+++ catalog/models.py
@@ -48,14 +48,10 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
-
     class Meta:
         ordering = ["last_name","first_name"]
-    
     def get_absolute_url(self):
         return reverse('author-detail', args=[str(self.id)])
-    
-
     def __str__(self):
         return '{0}, {1}'.format(self.last_name,self.first_name)
 
--- catalog/views.py
+++ catalog/views.py
@@ -15,3 +15,20 @@ def index(request):
         context={'num_books':num_books,'num_instances':num_instances,
                  'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author 
+
